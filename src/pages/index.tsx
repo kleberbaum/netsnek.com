afc19c7790a86e2bad7c760814b0bda3716c0538
Florian Kleber <kleber@snek.at>
2025-01-30 08:31:02 +0000

    refactor: update jaen and corresponding packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { PageConfig, PageProps } from '@atsnek/jaen';
+import { PageConfig, PageProps } from 'jaen';
 import { Global } from '@emotion/react';
 
 import { graphql } from 'gatsby';
@@ -71,4 +71,4 @@ export const query = graphql`
   }
 `;
 
-export { Head } from '@atsnek/jaen';
+export { Head } from 'jaen';
